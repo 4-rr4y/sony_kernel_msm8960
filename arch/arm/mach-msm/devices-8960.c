d7ec6ec1c5e83f876249e537e0136d059e061a5d
NuriJ <celiker.c4k@gmail.com>
2013-08-01 18:13:51 +0200

    added more gpu scaling steps

--- arch/arm/mach-msm/devices-8960.c
+++ arch/arm/mach-msm/devices-8960.c
@@ -2897,14 +2897,24 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
 #ifdef CONFIG_GPU_OVERCLOCK
 		{
+			.gpu_freq = 487500000,
+			.bus_freq = 6,
+			.io_fraction = 0,
+		},
+		{
 			.gpu_freq = 450000000,
-			.bus_freq = 4,
+			.bus_freq = 5,
 			.io_fraction = 0,
 		},
 		{
 			.gpu_freq = 400000000,
-			.bus_freq = 3,
+			.bus_freq = 4,
 			.io_fraction = 0,
+		},
+		{
+			.gpu_freq = 325000000,
+			.bus_freq = 3,
+			.io_fraction = 33,
 		},
 #else
 		{
@@ -2913,7 +2923,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 			.io_fraction = 0,
 		},
 		{
-			.gpu_freq = 300000000,
+			.gpu_freq = 325000000,
 			.bus_freq = 3,
 			.io_fraction = 33,
 		},
@@ -2934,7 +2944,11 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		},
 	},
 	.init_level = 1,
-	.num_levels = ARRAY_SIZE(grp3d_freq) + 1,
+#ifdef CONFIG_GPU_OVERCLOCK
+	.num_levels = 7,
+#else
+	.num_levels = 5,
+#endif
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/12,
 	.nap_allowed = true,

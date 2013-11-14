67d6be426062b00dfe2e55744f0dd84cc9ac030f
NuriJ <celiker.c4k@gmail.com>
2013-11-14 21:34:23 +0100

    force usb fast charge

--- drivers/usb/otg/msm_otg.c
+++ drivers/usb/otg/msm_otg.c
@@ -48,6 +48,11 @@
 #include <mach/msm_bus.h>
 #include <mach/rpm-regulator.h>
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#define USB_FASTCHG_LOAD 1000 /* uA */
+#endif
+
 #ifdef CONFIG_USB_HOST_EXTRA_NOTIFICATION
 #include <linux/usb/host_ext_event.h>
 #endif
@@ -1110,6 +1115,23 @@ static void msm_otg_notify_charger(struct msm_otg *motg, unsigned mA)
 		return;
 
 	dev_info(motg->phy.dev, "Avail curr from USB = %u\n", mA);
+
+        #ifdef CONFIG_FORCE_FAST_CHARGE
+        if (force_fast_charge == 1) {
+                // DooMLoRD: dont override charging current if available current is greater
+                if (mA >= USB_FASTCHG_LOAD){
+                        pr_info("Available current already greater than USB fastcharging current!!!\n");
+                        pr_info("Override of USB charging current cancelled.\n");
+                } else {                                
+                        mA = USB_FASTCHG_LOAD;
+                        pr_info("USB fast charging is ON!!!\n");
+                }
+                        dev_info(motg->phy.dev, "Avail curr from USB = %u\n", mA);
+
+        } else {
+                pr_info("USB fast charging is OFF.\n");
+        }
+#endif
 
 	/*
 	 *  Use Power Supply API if supported, otherwise fallback
